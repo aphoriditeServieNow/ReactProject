b7b706f954c7807696647c0a80921fdba1f7bf7c
Maha El Garce <61469906+mahalgarcee@users.noreply.github.com>
2024-05-31 23:14:24 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <User name="Test1" age={23} email="elgarcem@gmail.com" />
+      <User name="Test2" age={24} email="elgarcem@gmail.com" />
+      <User name="Test3" age={18} email="elgarcem@gmail.com" />
     </div>
   );
 }
 
+const GetName = () => { // normal Function
+  return "Maha";
+}
+
+
+const User = (props) => { // Component and need to start with Capital Letter
+
+  const user = (
+    <div>
+      <h1>{props.name}</h1>
+      <h2>{props.age}</h2>
+      <h3>{props.email}</h3>
+  </div>);
+  return <div>{user}</div>;
+}
 export default App;
